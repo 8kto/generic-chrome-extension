7d1eb7ea290f211c3fc4008d750d91294a3ffa0c
Igor Ivlev <igor.ivlev@verkstedt.com>
2021-12-13 14:27:30 +0100

    Render nice experiments list

--- components/popup/popup.js
+++ components/popup/popup.js
@@ -1,3 +1,5 @@
+/*global Optimizely, Template*/
+
 async function onPopupOpen() {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
 
@@ -8,21 +10,49 @@ async function onPopupOpen() {
       chrome.runtime.sendMessage({
         type: 'onPopupOpen',
         payload: document.cookie,
+        function(message) {
+          alert(JSON.stringify(message.type, null, '  '))
+        },
       })
     },
   })
 
-  chrome.runtime.onMessage.addListener(function (request) {
+  chrome.runtime.onMessage.addListener(function (
+    request,
+    sender,
+    sendResponse
+  ) {
     if (request.type !== 'onPopupOpen') {
+      alert('type: ' + request.type)
+
       return
     }
 
     const container = document.getElementById('container')
     const optimizelyService = new Optimizely(request.payload)
-    const experiments = optimizelyService.getExperiments()
+    const experiments = optimizelyService.extractExperiments()
 
-    const template = new Template()
-    template.renderExperimentsList(container, experiments)
+    const templateService = new Template()
+    const expListElement = templateService.renderExperimentsList(
+      container,
+      experiments
+    )
+
+    expListElement.addEventListener('click', function (event) {
+      /** @type {HTMLElement} */
+      const { target } = event
+
+      if (target.type === 'checkbox') {
+        const experiments = optimizelyService
+          .setExperimentStatus(target.value, target.checked)
+          .getExperiments()
+
+        sendResponse({
+          type: 'onExperimentStatusSet',
+          payload: experiments,
+        })
+      }
+    })
   })
 }
 
