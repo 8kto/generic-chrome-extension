985ac4091781014e8f308eb9d06585e2cea887d1
Igor Ivlev <igor.ivlev@verkstedt.com>
2021-12-13 12:22:03 +0100

    Refactor components, prepare for the services usage

--- components/popup/popup.js
+++ components/popup/popup.js
@@ -1,62 +1,28 @@
 async function onPopupOpen() {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-  const container = document.getElementById('container')
 
   chrome.scripting.executeScript({
-    args: [container],
+    args: [],
     target: { tabId: tab.id },
-    function: container => {
-      chrome.runtime.sendMessage({ greeting: 'hello' }, function (response) {
-        console.log(response.farewell)
+    function: () => {
+      chrome.runtime.sendMessage({
+        type: 'onPopupOpen',
+        payload: document.cookie,
       })
-
-      // FIXME find a way to include it from outside
-      class Optimizely {
-        getExperiments() {
-          let experiments = {}
-          const experimentsCookie = document.cookie
-            .split(';')
-            .filter(i => i.match(/feature-flag-cookie/))
-
-          if (experimentsCookie.length) {
-            const json = experimentsCookie.shift().replace(/^[^{]+/, '')
-
-            try {
-              experiments = JSON.parse(json)
-            } catch (error) {
-              console.error(error)
-            }
-          }
-
-          return experiments
-        }
-      }
-
-      function formatExperiment(experiment) {
-        const enabled = experiment.e ? 'Enabled' : 'Disabled'
-        const variation = experiment.v.v_name
-
-        return `${variation} ${enabled}`
-      }
-
-      function renderExperimentsList(container, experiments) {
-        const list = Object.entries(experiments).map(([name, def]) => {
-          return `${name}: ${formatExperiment(def)}`
-        })
-
-        const options = list.map(i => `<li>${i}</li>li>`)
-        const htmlRaw = `<ul>${options.join('\n')}</ul>`
-
-        container.append(htmlRaw)
-        console.log(list)
-      }
-
-      const optimizelyService = new Optimizely()
-      const experiments = optimizelyService.getExperiments()
-
-      debugger
-      renderExperimentsList(container, experiments)
     },
+  })
+
+  chrome.runtime.onMessage.addListener(function (request) {
+    if (request.type !== 'onPopupOpen') {
+      return
+    }
+
+    const container = document.getElementById('container')
+    const optimizelyService = new Optimizely(request.payload)
+    const experiments = optimizelyService.getExperiments()
+
+    const template = new Template()
+    template.renderExperimentsList(container, experiments)
   })
 }
 
