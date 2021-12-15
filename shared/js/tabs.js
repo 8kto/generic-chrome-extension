74a648de92b1b8b53e1213ec920601c5b3b43a5b
Igor Ivlev <igor.ivlev@verkstedt.com>
2021-12-15 12:19:22 +0100

    Introduce tabs

---
+++ shared/js/tabs.js
@@ -0,0 +1,24 @@
+const openTab = tabId => {
+  const tabContentElements = document.querySelectorAll('.tabContent')
+  tabContentElements.forEach(elem => {
+    elem.classList.remove('active')
+  })
+
+  const tabTitleElements = document.querySelectorAll('.tabTitle')
+  tabTitleElements.forEach(elem => elem.classList.remove('active'))
+
+  // document.getElementById(tabId).style.display = 'block'
+  document.getElementById(tabId).classList.add('active')
+}
+
+// eslint-disable-next-line no-unused-vars
+const initTabs = () => {
+  document.querySelectorAll('.tabTitle').forEach(link => {
+    link.addEventListener('click', event => {
+      const currentTarget = event.currentTarget
+
+      openTab(currentTarget.dataset.target)
+      currentTarget.classList.add('active')
+    })
+  })
+}
