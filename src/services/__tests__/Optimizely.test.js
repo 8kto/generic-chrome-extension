ed2ecd4bcd6cb9e3cc084d89a44009df4596ada4
Igor Ivlev <igor.ivlev@verkstedt.com>
2022-04-08 16:59:00 +0200

    Add tests for isFeatureFlagsValid

---
+++ src/services/__tests__/Optimizely.test.js
@@ -0,0 +1,58 @@
+import { Optimizely } from './../Optimizely'
+
+const VALID_FF_STRING = JSON.stringify({
+  'cro-691': {
+    'e': true,
+    'v': {
+      'v_name': 'v3',
+    },
+  },
+  'serp-filtering-v2': {
+    'e': true,
+    'v': {
+      'v_name': 'variation_2',
+    },
+  },
+})
+const VALID_COOKIE = `my-cookie=42; feature-flag-cookie=${VALID_FF_STRING}`
+
+describe('optimizely service', () => {
+  // Just to disable console spamming during tests
+  jest.spyOn(console, 'error').mockImplementation(() => undefined)
+
+  describe('isFeatureFlagsValid', () => {
+    /** @var {Optimizely} */
+    let optimizelyService
+
+    it('returns true for valid cookies', () => {
+      optimizelyService = new Optimizely(VALID_COOKIE)
+      expect(optimizelyService.isFeatureFlagsValid()).toBe(true)
+    })
+
+    it.each(['', '{}', 'null', 'undefined', '[]'])(
+      'returns false for empty and falsy FF cookies',
+      input => {
+        optimizelyService = new Optimizely(
+          `var=1;second=2;feature-flag-cookie=${input}`
+        )
+        expect(optimizelyService.isFeatureFlagsValid()).toBe(false)
+      }
+    )
+
+    it('throws when no FF cookies', () => {
+      optimizelyService = new Optimizely('var=1;second=2')
+      expect(() => optimizelyService.isFeatureFlagsValid()).toThrow(
+        'No feature-flag-cookie found'
+      )
+    })
+
+    it('throws when multiple FF cookies', () => {
+      optimizelyService = new Optimizely(
+        `my-cookie=42; feature-flag-cookie=${VALID_FF_STRING}; feature-flag-cookie={}`
+      )
+      expect(() => optimizelyService.isFeatureFlagsValid()).toThrow(
+        'Ambiguous feature-flag-cookie found: remove multiple values and reload tab'
+      )
+    })
+  })
+})
