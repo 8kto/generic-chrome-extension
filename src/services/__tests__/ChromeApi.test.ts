3f93c9b2058b9722185dbbd8e2e61b2040d6f86b
Igor Ivlev <igor.ivlev@verkstedt.com>
2022-05-21 13:43:35 +0200

    test: extend tests

--- src/services/__tests__/ChromeApi.test.ts
+++ src/services/__tests__/ChromeApi.test.ts
@@ -9,14 +9,19 @@ describe('chrome API service', () => {
       typeof ChromeApiHelpers.getGlobalChromeApi
     >
 
-  describe('getActiveTabId', () => {
-    const query = jest
-      .fn()
-      .mockImplementation(() => Promise.resolve([{ id: 42 }]))
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
 
-    getGlobalChromeApi.mockReturnValue({
-      //@ts-ignore
-      tabs: { query },
+  describe('getActiveTabId', () => {
+    let query: jest.Mock<unknown>
+
+    beforeEach(() => {
+      query = jest.fn().mockImplementation(() => Promise.resolve([{ id: 42 }]))
+      getGlobalChromeApi.mockReturnValue({
+        //@ts-ignore
+        tabs: { query },
+      })
     })
 
     it('calls API', async () => {
@@ -28,6 +33,95 @@ describe('chrome API service', () => {
       })
 
       await expect(result).resolves.toBe(42)
+    })
+  })
+
+  describe('reloadTab', () => {
+    let reload: jest.Mock<unknown>
+    let close: jest.Mock<unknown>
+
+    beforeEach(() => {
+      reload = jest.fn().mockImplementation(() => Promise.resolve())
+      close = jest.fn()
+
+      jest.spyOn(window, 'close').mockImplementation(close)
+      getGlobalChromeApi.mockReturnValue({
+        //@ts-ignore
+        tabs: { reload },
+      })
+    })
+
+    it('calls API', async () => {
+      const result = ChromeApi.reloadTab(777)
+      expect(result).toBeInstanceOf(Promise)
+      expect(reload).toHaveBeenCalledWith(777)
+
+      expect(close).toHaveBeenCalledTimes(0)
+      await expect(result).resolves.toBeUndefined()
+      expect(close).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('executeScript', () => {
+    let executeScript: jest.Mock<unknown>
+
+    beforeEach(() => {
+      getGlobalChromeApi.mockReset()
+      if (executeScript) {
+        executeScript.mockReset()
+      }
+
+      executeScript = jest.fn().mockImplementation((_, cb) => {
+        return Promise.resolve([{ result: 'test' }]).then(cb)
+      })
+
+      getGlobalChromeApi.mockReturnValue({
+        //@ts-ignore
+        scripting: { executeScript },
+      })
+    })
+
+    it('calls API', async () => {
+      const result = ChromeApi.executeScript({
+        args: [1, 'test string'],
+        target: { tabId: 888 },
+        function: () => undefined,
+      })
+
+      expect(result).toBeInstanceOf(Promise)
+      expect(executeScript).toHaveBeenCalledWith(
+        {
+          args: [1, 'test string'],
+          target: { tabId: 888 },
+          function: expect.any(Function),
+        },
+        undefined
+      )
+    })
+
+    it('calls API and passes callback', async () => {
+      const callback = jest.fn()
+      const result = ChromeApi.executeScript(
+        {
+          args: [1, 'test string'],
+          target: { tabId: 888 },
+          function: () => undefined,
+        },
+        callback
+      )
+
+      expect(executeScript).toHaveBeenCalledWith(
+        {
+          args: [1, 'test string'],
+          target: { tabId: 888 },
+          function: expect.any(Function),
+        },
+        callback
+      )
+
+      expect(callback).toHaveBeenCalledTimes(0)
+      await result
+      expect(callback).toHaveBeenCalledTimes(1)
     })
   })
 })
