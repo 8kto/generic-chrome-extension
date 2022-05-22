cf02ac27de2a1e5b445131128459092f929be0a0
Okto <web@axisful.info>
2022-05-22 12:11:08 +0200

    refactor: extract classnames and selectors as consts

--- src/shared/js/tabs.ts
+++ src/shared/js/tabs.ts
@@ -1,23 +1,27 @@
+const SELECTOR_TAB_CONTENT = '.tabContent'
+const SELECTOR_TAB_TITLE = '.tabTitle'
+const CLASS_ACTIVE = 'active'
+
 const openTab = (tabId: string): void => {
-  const tabContentElements = document.querySelectorAll('.tabContent')
+  const tabContentElements = document.querySelectorAll(SELECTOR_TAB_CONTENT)
   tabContentElements.forEach(elem => {
-    elem.classList.remove('active')
+    elem.classList.remove(CLASS_ACTIVE)
   })
 
-  const tabTitleElements = document.querySelectorAll('.tabTitle')
-  tabTitleElements.forEach(elem => elem.classList.remove('active'))
+  const tabTitleElements = document.querySelectorAll(SELECTOR_TAB_TITLE)
+  tabTitleElements.forEach(elem => elem.classList.remove(CLASS_ACTIVE))
 
-  document.getElementById(tabId).classList.add('active')
+  document.getElementById(tabId).classList.add(CLASS_ACTIVE)
 }
 
 export const initTabs = (): void =>
-  document.querySelectorAll('.tabTitle').forEach(link => {
+  document.querySelectorAll(SELECTOR_TAB_TITLE).forEach(link => {
     link.addEventListener('click', event => {
       const currentTarget = <HTMLElement>event.currentTarget
 
       if (currentTarget.dataset.target) {
         openTab(currentTarget.dataset.target)
-        currentTarget.classList.add('active')
+        currentTarget.classList.add(CLASS_ACTIVE)
       }
     })
   })
