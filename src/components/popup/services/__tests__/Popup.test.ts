e017437f0cd16c3fc536fbc170bb8a61bd42ae96
Okto <web@axisful.info>
2022-05-22 12:39:03 +0200

    test: cover migrated methods

--- src/components/popup/services/__tests__/Popup.test.ts
+++ src/components/popup/services/__tests__/Popup.test.ts
@@ -1,3 +1,5 @@
+import ChromeApi from 'services/ChromeApi'
+
 import Popup from './../Popup'
 
 describe('popup service test', () => {
@@ -70,6 +72,52 @@ describe('popup service test', () => {
         'v6',
         'Custom',
       ])
+    })
+  })
+
+  describe('updateExtensionVersion', () => {
+    it('updates the version container', () => {
+      document.body.innerHTML = `<p><strong>Version</strong>: <span id="igel-version"></span></p>`
+      jest
+        .spyOn(ChromeApi, 'getManifest')
+        // @ts-ignore
+        .mockReturnValue({ version: 'v1.1.1' })
+
+      const versionContainer = document.getElementById('igel-version')
+
+      expect(versionContainer.innerHTML).toBe('')
+      Popup.updateExtensionVersion()
+      expect(versionContainer.innerHTML).toBe('v1.1.1')
+
+      jest.resetAllMocks()
+    })
+  })
+
+  describe('updateDetailsTabContent', () => {
+    it('updates the details', () => {
+      document.body.innerHTML =
+        `<span id="feature-branch-container">NA</span>` +
+        `<span id="feature-flag-targeting-params-container">NA</span>`
+
+      const featureBranchContainer = document.getElementById(
+        'feature-branch-container'
+      )
+      const targetingParamsContainer = document.getElementById(
+        'feature-flag-targeting-params-container'
+      )
+
+      expect(targetingParamsContainer.innerHTML).toBe('NA')
+      expect(featureBranchContainer.innerHTML).toBe('NA')
+
+      Popup.updateDetailsTabContent(
+        [
+          `x-featurebranch=TestBranch`,
+          `feature-flag-targeting={"browser-family":"Chrome","browser-version":"101.0.4951.54","device":"mobile","expa":"","isInternalIP":false,"tenant":"com"};`,
+        ].join('; ')
+      )
+
+      expect(featureBranchContainer.innerHTML).toBe('TestBranch')
+      expect(targetingParamsContainer.innerHTML).toMatchSnapshot()
     })
   })
 })
