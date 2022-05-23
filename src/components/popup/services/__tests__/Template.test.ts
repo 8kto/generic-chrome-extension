4b0d3628163c7b6502dbec21e1ff65a5bcff9478
Igor Ivlev <igor.ivlev@verkstedt.com>
2022-05-23 12:41:29 +0200

    test: add dummy tests to display the common project coverage during the `yarn test` running

---
+++ src/components/popup/services/__tests__/Template.test.ts
@@ -0,0 +1,7 @@
+import Template from './../Template'
+
+describe('template service tests', () => {
+  it('should be defined', () => {
+    expect(Template).toBeDefined()
+  })
+})
---
+++ src/components/popup/services/__tests__/VariableUpdate.test.ts
@@ -0,0 +1,7 @@
+import VariableUpdate from './../VariableUpdate'
+
+describe('variable update service tests', () => {
+  it('should be defined', () => {
+    expect(VariableUpdate).toBeDefined()
+  })
+})
